d90b28fd94e75a619008d6352dca90d44520215f
Mateusnasciment <mateus.nasciment@hotmail.com>
2022-09-22 19:24:37 +0000

    first

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+
+function App() {
+return (
+    <div>
+      <center>
+        <h1 className="divmain">
+          <img src="https://i.pinimg.com/originals/b0/ec/56/b0ec56b4b040a52c6670732823b157f3.gif" alt="3d"></img>
+          <li></li>
+          <img src="http://2.bp.blogspot.com/-wnnQre1_znE/Ua6B9jEBnyI/AAAAAAAAAS8/cT0Dq4J1hM8/s1600/tumblr_mmyrl6NF0K1qm37tio1_500.gif"></img>
+          <li></li>
+          <img src="https://vejasp.abril.com.br/wp-content/uploads/2016/12/tumblr_ma86cwi7fu1r69okeo1_500.gif"></img>
+        </h1>
+      </center>
+    </div>
+    );
+}
+      
+
+export default App;
